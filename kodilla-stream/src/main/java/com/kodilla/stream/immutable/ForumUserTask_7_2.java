b6b33b17993bf3c1c08a1d5d927aad41e9c29718
Robert Głuszkiewicz <robert@macbookpro.home>
2020-11-29 21:36:55 +0100

    Added module 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUserTask_7_2.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUserTask_7_2.java
@@ -1,10 +1,10 @@
 package com.kodilla.stream.immutable;
 
-public final class ForumUser {
+public final class ForumUserTask_7_2 {
     private final String userName;
     private final String realName;
 
-    public ForumUser(final String userName, final String realName) {
+    public ForumUserTask_7_2(final String userName, final String realName) {
         this.userName = userName;
         this.realName = realName;
     }
