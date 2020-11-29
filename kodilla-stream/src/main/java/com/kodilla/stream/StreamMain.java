67ab05b822bf0969419d544770c355fcfe7715e3
Robert Głuszkiewicz <robert@macbookpro.home>
2020-11-29 18:51:35 +0100

    Added module 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,31 +1,70 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        BookDirectory theBookDirectory = new BookDirectory();
+
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));             // [1]
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());             // [2]
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())                   // [3]
+                .forEach(System.out::println);                                            // [4]
     }
 }
-
-
-
-
-
-
-
 
 /**
 package com.kodilla.stream;
 
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        BookDirectory theBookDirectory = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()   // [1]
+                .filter(book -> book.getYearOfPublication() > 2005)                  // [2]
+                .collect(Collectors.toList());                                       // [3]
+
+        System.out.println("# elements: " + theResultListOfBooks.size());       // [4]
+        theResultListOfBooks.stream()                                           // [5]
+                .forEach(System.out::println);                                       // [6]
+    }
+}
+*/
+
+/**
+package com.kodilla.stream;
+
+import com.kodilla.stream.book.BookDirectory;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
+    }
+}
+
+*/
+
+/**
+package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
-
 public class StreamMain {
-
     public static void main(String[] args) {
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         poemBeautifier.beautify("This is an example text", (inputText) -> "ABC " + inputText + " ABC");
@@ -34,23 +73,17 @@ public class StreamMain {
         poemBeautifier.beautify("This is an example text", (inputText) -> inputText.replace(" ", "_"));
     }
 }
-
 package com.kodilla.stream;
-
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
-
 public class StreamMain {
-
     public static void main(String[] args) {
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
         System.out.println("Calculating expressions with lambdas");
         expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
         System.out.println("Calculating expressions with method references");
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
