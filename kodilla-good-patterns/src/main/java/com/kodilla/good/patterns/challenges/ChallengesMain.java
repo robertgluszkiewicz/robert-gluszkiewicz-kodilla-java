01f3cd2b5d8799d943bdf57b012836d12a9036b2
Robert Głuszkiewicz <robert@macbookpro.home>
2021-02-07 19:26:44 +0100

    Added 13.1 task

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ChallengesMain.java
@@ -0,0 +1,14 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class ChallengesMain {
+    public static void main(String[] args) {
+
+        String movies = MovieStore.getMovies().entrySet().stream()
+                .flatMap(title -> title.getValue().stream())
+                .collect(Collectors.joining("!","", ""));
+
+        System.out.println(movies);
+    }
+}
