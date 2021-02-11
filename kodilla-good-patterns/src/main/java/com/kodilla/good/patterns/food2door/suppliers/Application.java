76a5c79ed1a0c00aa98decbdf08cef4da736b6d3
Robert Głuszkiewicz <robert@macbookpro.home>
2021-02-11 08:54:57 +0100

    Corrected task 13.2 #2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/suppliers/Application.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.good.patterns.food2door.suppliers;
-
-public class Application {
-
-    public static void main(String[] args) {
-
-    }
-}
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/suppliers/SupplierAdding.java
+++
@@ -1,6 +0,0 @@
-package com.kodilla.good.patterns.food2door.suppliers;
-
-public interface SupplierAdding {
-
-    public boolean process();
-}
