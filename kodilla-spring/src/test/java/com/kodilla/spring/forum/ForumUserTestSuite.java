299030bc8ff14ed9275602b2a7f64a4468a1690d
Robert Głuszkiewicz <robert@macbookpro.home>
2021-02-16 00:15:11 +0100

    Correctet task 14.1

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -1,11 +1,13 @@
 package com.kodilla.spring.forum;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 public class ForumUserTestSuite {
 
     @Test
