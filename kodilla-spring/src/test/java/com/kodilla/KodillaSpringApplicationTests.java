687f0449ec15e46556015de50b5a9db7c845d166
Robert Głuszkiewicz <robert@macbookpro.home>
2021-02-18 14:45:40 +0100

    Corrected setting.gradle #2

--- kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
@@ -1,9 +1,5 @@
-package com.kodilla.spring.shape;
-
-import com.kodilla.spring.shape.Circle;
+package com.kodilla;
 import com.kodilla.spring.shape.Shape;
-import com.kodilla.spring.shape.Square;
-import com.kodilla.spring.shape.Triangle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
